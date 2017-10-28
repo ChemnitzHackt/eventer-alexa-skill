4ea152d8392aacc4b9e6c0cc4b7b1ab4c6bd0740
Felix Bytow <drako@drako.guru>
2017-10-28 15:50:33 +0200

    :loud_sound: logging the alexa request now

--- src/main/kotlin/guru/drako/eventer/alexa/controllers/AlexaController.kt
+++ src/main/kotlin/guru/drako/eventer/alexa/controllers/AlexaController.kt
@@ -2,6 +2,8 @@ package guru.drako.eventer.alexa.controllers
 
 import guru.drako.eventer.alexa.data.AlexaRequest
 import guru.drako.eventer.alexa.data.AlexaResponse
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
@@ -9,8 +11,13 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class AlexaController {
+  companion object {
+    private val logger: Logger = LoggerFactory.getLogger(AlexaController::class.java)
+  }
+
   @RequestMapping("api/alexa/eventer", method = [RequestMethod.POST])
   fun eventerSkill(@RequestBody alexaRequest: AlexaRequest) : AlexaResponse {
+    logger.info("Got request: {}", alexaRequest)
     return AlexaResponse("Hello world")
   }
 }
